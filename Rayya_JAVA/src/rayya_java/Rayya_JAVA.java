374fdcfd57144ecc0d71e9d73e957da377ad9762
Aryaarayya <rayyaarya41@gmail.com>
2025-02-13 11:00:25 +0700

    Update Rayya_JAVA.java
    Awal

--- Rayya_JAVA/src/rayya_java/Rayya_JAVA.java
+++ Rayya_JAVA/src/rayya_java/Rayya_JAVA.java
@@ -4,13 +4,15 @@ package rayya_java;
 
 public class Rayya_JAVA {
     public static void main(String[] args) {
-        String fName = "Lisa Palombo";
-        String IName = "123456789";
+        String fName = "Lisa";
+        String IName = "Palombo";
+        String stuId = "123456789";
         String stuStatus = "Active";
         
-        System.out.println("Student Name: " + fName);
-        System.out.println("Student ID: " + IName);
-        System.out.println("Student Status: " + stuStatus);
+        System.out.println("Name Awal: " + fName);
+        System.out.println("Nama Akhir: " + IName);
+        System.out.println("ID: " + stuId);
+        System.out.println("Status: " + stuStatus);
     }
     
 }
