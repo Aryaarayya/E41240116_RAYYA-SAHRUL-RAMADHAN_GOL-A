a8a3e17948e00e5238251cc0203ed08c168ffeee
Aryaarayya <rayyaarya41@gmail.com>
2025-02-25 05:01:29 +0700

    Update Person.java

--- Rayya_JAVA/src/rayya_java/Person.java
+++ Rayya_JAVA/src/rayya_java/Person.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rayya_java;
 
-/**
- *
- * @author Rumah Laptop
- */
 public class Person {
     private String name;
     private int age;
