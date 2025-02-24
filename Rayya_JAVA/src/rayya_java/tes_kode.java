33ca52e3813235049f44d56c1d88fc55e5961bce
Aryaarayya <rayyaarya41@gmail.com>
2025-02-25 05:01:34 +0700

    Create tes_kode.java

---
+++ Rayya_JAVA/src/rayya_java/tes_kode.java
@@ -0,0 +1,19 @@
+
+package rayya_java;
+
+
+public class tes_kode {
+    
+    
+    boolean gameOver = false;
+    int students=50, classes=3;
+    double sales_tax;
+    short number1;
+    
+    int 2beOrNot2be; //Variabel tidak boleh dimulai dengan awalan angka
+    float price index; //Variabel tidak boleh ada spasi, untuk gantinya spasi diganti dengan 
+                       //underscore
+    double lastYear'sPrice; //Dalam variable tidak boleh menggunakan tanda petik
+    long class; // class adalah nama yang sudah digunakan pada java, jadi tidak dapat digunakan 
+                // lagi pada variable
+}
