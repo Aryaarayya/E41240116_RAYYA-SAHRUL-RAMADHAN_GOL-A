588c7ff49c4c7f671672d57198f6813765c13613
Aryaarayya <rayyaarya41@gmail.com>
2025-03-04 21:34:01 +0700

    Create Dog.java

---
+++ Rayya_JAVA/src/rayya_java/Dog.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rayya_java;
+public class Dog extends Animal {
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
+}
