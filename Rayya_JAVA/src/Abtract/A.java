3b0ca0d6690c89092c86cfaa0df0ffe1becdca4d
Aryaarayya <rayyaarya41@gmail.com>
2025-03-06 09:09:29 +0700

    Abstrsct

---
+++ Rayya_JAVA/src/Abtract/A.java
@@ -0,0 +1,11 @@
+
+package Abtract;
+
+public abstract class A { // Deklarasi Class
+    abstract public void method2(); // Deklarasi method abstrak.
+
+    // Method konkrit yang memiliki implementasi
+    public void method1() {
+        System.out.println("Method konkrit dari class A");
+    }
+}
